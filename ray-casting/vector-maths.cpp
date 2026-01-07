8aa4ac11e888a44f202a14e024e237a387d7949d
anand <anandvaibhaw000@gmail.com>
2026-01-07 18:13:06 +0530

    fix vecotr-maths module

--- ray-casting/vector-maths.cpp
+++ ray-casting/vector-maths.cpp
@@ -1,30 +1,35 @@
-#include<cmath>
-class Vec3 {
-  float a,b,c;
-}
+#include <cmath>
+
+struct Vec3 {
+    float a, b, c;
+};
 
 Vec3 add(Vec3 x, Vec3 y) {
-  return x.a + y.a, x.b + y.b, x.c + y.c; 
+    return { x.a + y.a, x.b + y.b, x.c + y.c };
 }
 
 Vec3 sub(Vec3 x, Vec3 y) {
-  return x.a - y.a, x.b - y.b, x.c - y.c;
+    return { x.a - y.a, x.b - y.b, x.c - y.c };
 }
 
 Vec3 mul(Vec3 x, float z) {
-  return x.a * z, x.b * z, x.c * z; 
+    return { x.a * z, x.b * z, x.c * z };
 }
 
 float dot(Vec3 x, Vec3 y) {
-  return x.a * y.a + x.b * y.b + x.c * y.c;
+    return x.a * y.a + x.b * y.b + x.c * y.c;
 }
 
 Vec3 cross(Vec3 x, Vec3 y) {
-  return x.a * (y.a + y.b + y.c), -1 * x.b * (y.a + y.b + y.c), x.c * (y.a + y.b + y.c)
+    return {
+        x.b * y.c - x.c * y.b,
+        x.c * y.a - x.a * y.c,
+        x.a * y.b - x.b * y.a
+    };
 }
 
 Vec3 normalize(Vec3 x) {
-  float length = sqrt(dot(x,x))
-  return x.a/length, x.b/length, x.c/length;
+    float length = std::sqrt(dot(x, x));
+    if (length == 0.0f) return {0, 0, 0};
+    return { x.a / length, x.b / length, x.c / length };
 }
-
